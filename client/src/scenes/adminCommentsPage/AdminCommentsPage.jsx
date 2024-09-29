2f58c34c255932d79a201d9a7d3fe3408d8358e8
ShaneMalcolm <shanemalcolm02@gmail.com>
2024-09-29 14:42:15 +0530

    Commit

--- client/src/scenes/adminCommentsPage/AdminCommentsPage.jsx
+++ client/src/scenes/adminCommentsPage/AdminCommentsPage.jsx
@@ -90,6 +90,18 @@ const AdminCommentsPage = () => {
                 <Typography sx={{ color: palette.neutral.main, mb: "1rem" }}>
                   Post ID: {comment.postId}
                 </Typography>
+                <Typography sx={{ color: palette.neutral.main, mb: "1rem" }}>
+                  Description: {comment.description}
+      </Typography>
+      {comment.picturePath && (
+        <img
+          width="100%"
+          height="auto"
+          alt="post"
+          style={{ borderRadius: "0.75rem", marginTop: "0.75rem" }}
+          src={`http://localhost:3001/assets/${comment.picturePath}`}
+        />
+      )}
                 <Button
                   variant="contained"
                   color="primary"
